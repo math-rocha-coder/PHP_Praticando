8432aa8cd85f10f21c78eeeccc37e38a1545cc4c
matheus-coder <69425699+matheus-coder@users.noreply.github.com>
2022-04-15 20:02:16 -0300

    Código aleatório para fixar estruturas do PHP

---
+++ Index.php
@@ -0,0 +1,28 @@
+<?php
+//Variavel
+// uma variavel não pode iniciar com numeros, mas com _ pode 
+$cor = "azul";
+$cor = "vermelho";
+echo  "<p>$cor</p>";
+
+
+$x = 10;
+$y = 5;
+
+function teste(){
+    global $x; // como declarar uma variavel global
+    echo "<h1>O valor de x dentro da Função $x</h1>";
+}
+
+teste();
+echo "<h2>O valor do x fora da função $x</h2>";
+
+
+
+//manipulação de stings 
+
+$palavra = "matheus Gomes da Rocha";
+echo strlen ();
+
+
+?>
